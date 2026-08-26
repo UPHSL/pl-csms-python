16b17c2067724a8b0f5b070d7b2031e184a4d6e5
Jerstin Mane <jerstinmane2@gmail.com>
2026-08-26 11:38:33 +0800

    feat: define resident domain model

--- src/csms/models/resident.py
+++ src/csms/models/resident.py
@@ -1,4 +1,18 @@
-"""Resident model placeholder.
-
-The Resident domain model will be implemented in CSMS-201.
-"""
+class Resident:
+    def __init__(
+        self,
+        first_name,
+        last_name,
+        address,
+        contact_number,
+        email,
+        status="Active",
+        id=None,
+    ):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.contact_number = contact_number
+        self.email = email
+        self.status = status
